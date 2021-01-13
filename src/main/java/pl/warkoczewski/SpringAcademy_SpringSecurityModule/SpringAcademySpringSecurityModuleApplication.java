db4feff62cc7881fda7a6087a22e15b01c4afc40
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-01-13 17:43:32 +0100

    initial commit

---
+++ src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/SpringAcademySpringSecurityModuleApplication.java
@@ -0,0 +1,13 @@
+package pl.warkoczewski.SpringAcademy_SpringSecurityModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcademySpringSecurityModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAcademySpringSecurityModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/SpringAcademySpringSecurityModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.warkoczewski.SpringAcademy_SpringSecurityModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcademySpringSecurityModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
