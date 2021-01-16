259805bcd5142e0c4832889b3a6a135acef5dbaf
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-01-16 20:34:16 +0100

    UserDetailsServiceImpl

---
+++ src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package pl.warkoczewski.SpringAcademy_SpringSecurityModule;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+
+    public WebSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
---
+++ src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package pl.warkoczewski.SpringAcademy_SpringSecurityModule.service.impl;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.warkoczewski.SpringAcademy_SpringSecurityModule.repository.UserRepository;
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
