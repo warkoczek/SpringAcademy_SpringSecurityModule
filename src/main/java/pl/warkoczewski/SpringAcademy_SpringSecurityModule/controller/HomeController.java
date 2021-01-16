85546db23891a34779046c60c9e16aa388d6bd5c
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-01-16 20:20:37 +0100

    HomeController and RegistrationController

---
+++ src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/controller/HomeController.java
@@ -0,0 +1,15 @@
+package pl.warkoczewski.SpringAcademy_SpringSecurityModule.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    @GetMapping("/home")
+    public String displayHomePage(){
+        return "/home/homePage";
+    }
+
+}
---
+++ src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/controller/RegistrationController.java
@@ -0,0 +1,14 @@
+package pl.warkoczewski.SpringAcademy_SpringSecurityModule.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+    @GetMapping
+    public String displayRegistrationPage(){
+        return "/register/sign_up";
+    }
+}
