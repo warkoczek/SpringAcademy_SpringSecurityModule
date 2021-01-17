ba6219d066033cd8a3208b3019e0250a38c4204c
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-01-17 08:53:59 +0100

    signed_inAll.html

--- src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/config/WebSecurityConfig.java
+++ src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/config/WebSecurityConfig.java
@@ -21,6 +21,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.authorizeRequests()
+        .antMatchers("/home").permitAll()
+        .antMatchers("/search").hasAnyRole("USER", "ADMIN", "ADMIN_HEAD");
     }
 }
