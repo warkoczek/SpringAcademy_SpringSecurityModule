35e764d93d3830349159a00a51703f6041f28df3
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-02-09 18:56:29 +0100

    jwt dependency

--- src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/controller/RestaurantController.java
+++ src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/controller/RestaurantController.java
@@ -1,7 +1,34 @@
 package pl.warkoczewski.SpringAcademy_SpringSecurityModule.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jdk.net.SocketFlow;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.warkoczewski.SpringAcademy_SpringSecurityModule.model.City;
+import pl.warkoczewski.SpringAcademy_SpringSecurityModule.model.Restaurant;
+import pl.warkoczewski.SpringAcademy_SpringSecurityModule.service.RestaurantService;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class RestaurantController {
+po    private final RestaurantService restaurantService;
+
+    @GetMapping("/restaurantsByCity/{city}")
+    public ResponseEntity<List<Restaurant>> displayAllRestaurantsByCity(@PathVariable String city){
+        List<Restaurant> restaurants = restaurantService.listRestaurantsByCity(City.valueOf(city));
+        if(restaurants.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(restaurants, HttpStatus.OK);
+    }
+    @PostMapping("/addRestaurant")
+    public ResponseEntity<Restaurant> postRestaurant(@RequestBody Restaurant restaurant){
+        if(restaurant != null){
+            return new ResponseEntity<>(restaurantService.addRestaurant(restaurant), HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
