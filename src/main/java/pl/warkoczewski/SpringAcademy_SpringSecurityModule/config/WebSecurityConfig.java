40deee594acc2965e0481a960ca9273b084c7198
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-02-09 19:09:30 +0100

    WebSecurityConfig

---
+++ src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/config/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package pl.warkoczewski.SpringAcademy_SpringSecurityModule.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/restaurantsByCity/{city}").authenticated();
+    }
+}
