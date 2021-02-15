1a05ffdddd93887693a9c3705e6c2e2ae9764074
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-02-15 10:33:56 +0100

    DefaultRestaurantService implemented methods

--- src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/filter/JwtFilter.java
+++ src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/filter/JwtFilter.java
@@ -5,11 +5,10 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
+
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
--- src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/service/DefaultRestaurantService.java
+++ src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule/service/DefaultRestaurantService.java
@@ -2,18 +2,24 @@ package pl.warkoczewski.SpringAcademy_SpringSecurityModule.service;
 
 import org.springframework.stereotype.Service;
 import pl.warkoczewski.SpringAcademy_SpringSecurityModule.model.Restaurant;
+import pl.warkoczewski.SpringAcademy_SpringSecurityModule.repository.RestaurantRepository;
 
 import java.util.List;
 @Service
 public class DefaultRestaurantService implements RestaurantService {
+    private final RestaurantRepository restaurantRepository;
+
+    public DefaultRestaurantService(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
 
     @Override
     public List<Restaurant> listRestaurants() {
-        return null;
+        return restaurantRepository.findAll();
     }
 
     @Override
     public Restaurant addRestaurant(Restaurant restaurant) {
-        return null;
+        return restaurantRepository.save(restaurant);
     }
 }
